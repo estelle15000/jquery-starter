17ee0994cec4827845a399a11b3a834d08325a35
Nicolas HULEUX <nicolas.huleux@gmail.com>
2020-05-15 10:30:11 +0200

    '2'

--- app.js
+++ app.js
@@ -819,9 +819,11 @@ app.post('/getUserMessages', function(req, res, next) {
 
 
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 
-
-app.listen(80, function() {
-	console.log('Example app listening on port 80!')
-})
+// app.listen(80, function() {
+// 	console.log('Example app listening on port 80!')
+// })
